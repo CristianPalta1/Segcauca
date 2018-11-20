faa13c7532fe41c3b382fd794bb62f21d194cff0
CristianPalta1 <cristian.palta.c@uniautonoma.edu.co>
2018-11-20 11:45:25 -0500

    segundo_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+const rutas: Routes = [
+  {path: '', component : InicioComponent},
+  {path: 'inicio', component : InicioComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component ({
+  selector: 'app-inicio',
+  templateUrl: './app.inicio.html',
+  styleUrls: ['./app.inicio.css']
+})
+export class InicioComponent {
+  title = 'SegCauca';
+}
